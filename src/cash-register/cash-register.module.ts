876e2fad46c23b3e62d166864aa9f32ea2042af2
Tharsiegan <tharsiegan2001@gmail.com>
2026-08-31 17:12:27 +0530

    Import PermissionsModule into CashRegisterModule

--- src/cash-register/cash-register.module.ts
+++ src/cash-register/cash-register.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CashRegisterService } from './cash-register.service';
 import { CashRegisterController } from './cash-register.controller';
+import { PermissionsModule } from '../permissions/permissions.module';
 
 @Module({
+  imports: [PermissionsModule],
   controllers: [CashRegisterController],
   providers: [CashRegisterService],
   exports: [CashRegisterService],
